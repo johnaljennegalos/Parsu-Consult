e2a4d3d29ce050807a2f72f3db8b55773517f822
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-09-10 19:54:10 +0800

    feat: User serializer
    Created 3 seperate serailizers for User
    -General serializer for general info
    -StudentRegistrationSerializer for Student registration only
    -InstructorRegistrationSerializer for instructors

---
+++ backend/api/serializers.py
@@ -0,0 +1,89 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from .models import User
+
+
+class UserGeneralSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'email',
+            'role',
+            'address',
+            'contact_number',
+            'first_name',
+            'last_name',
+            'student_id',
+            'course',
+            'year_level',
+            'section',
+            'department',
+            'specialization',
+            'employee_id',
+        ]
+
+        extra_kwargs = {
+            'role' : {'read_only' : True},
+            'student_id' : {'read_only' : True},
+            'employee_id' : {'read_only' : True}
+        }
+
+
+class StudentRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'student_id',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'contact_number',
+            'address',
+            'course',
+            'year_level',
+            'section'
+        ]
+
+        extra_kwargs = {
+            'password' : {'write_only' : True},
+            'student_id' : {'required' : True},
+            'course' : {'required' : True},
+            'year_level' : {'required' : True},
+            'section' : {'required' : True}
+        }
+
+    def create(self, validated_data):
+        validated_data['role'] = 'ST'
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class InstructorRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'employee_id',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'contact_number',
+            'address',
+            'department',
+            'specialization'
+        ]
+
+        extra_kwargs = {
+            'password' : {'write_only' : True},
+            'employee_id' : {'required' : True},
+            'department' : {'required' : True}
+        }
+
+    def create(self, validated_data):
+        validated_data['role'] = 'IN'
+        user = User.objects.create_user(**validated_data)
+        return user
