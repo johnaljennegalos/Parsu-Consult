d67c7d70b02c4793b69e74e878e3eea26fa7d78d
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-09-13 22:27:21 +0800

    feat: implement permissions
    -IsInstructor, enforces strict instructor-only access globally.
    -IsStudent: Enforces strict student-only access globally.
    -IsInstructorOrReadOnly: Public GET access; POST/PUT/DELETE restricted to instructors.
    IsStudentOrReadOnly: Public GET access; POST/PUT/DELETE restricted to students.
    IsRecipientOnly: Ensures notification object privacy (obj.recipient == request.user).
    IsAppointmentParticipant: Ensures multi-party appointment privacy (student or instructor)

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'rest_framework_simplejwt',
     'rest_framework',
 ]
 
@@ -129,3 +131,20 @@ MAILERS = {
 }
 
 AUTH_USER_MODEL = 'api.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
